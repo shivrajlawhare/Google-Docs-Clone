961ec6e9af7b1a47aa3fcccacaa5be48ac93b83f
shivrajlawhare <shivrajlawhare6@gmail.com>
2024-08-07 01:47:45 +0530

    collaborative room not added

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,11 +1,24 @@
 
+import CollaborativeRoom from '@/components/CollaborativeRoom'
+import { getDocument } from '@/lib/actions/room.actions';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const Document = () => {
+const Document = async ({params: {id}}: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if(!clerkUser) redirect('/sign-in')
+
+    const room = await getDocument({roomId:id, userId: clerkUser.id})
+
+    if(!room) redirect('/')
   return (
-    <div>
-      
-    </div>
+    <main className='flex flex-col w-full items-center'>
+      <CollaborativeRoom 
+        roomId={id}
+        roomMetadata={room.metadata}
+      />
+    </main>
   )
 }
 
--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -3,45 +3,33 @@ import { getUserColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
-
 export async function POST(request: Request) {
-    const clerkUser = await currentUser()
-    if (!clerkUser) {
-        redirect("/sign-in")
-    }
+  const clerkUser = await currentUser();
 
-    const { id, firstName, lastName, emailAddresses, imageUrl, publicMetadata } = clerkUser;
+  if(!clerkUser) redirect('/sign-in');
 
-    const organization = publicMetadata?.organization || "defaultOrg"; // Replace with actual organization logic
-  const group = publicMetadata?.group || "defaultGroup";
+  const { id, firstName, lastName, emailAddresses, imageUrl } = clerkUser;
 
   // Get the current user from your database
   const user = {
-    id: id,
-    organization: organization,
-    group: group,
+    id,
     info: {
-      id: id,
+      id,
       name: `${firstName} ${lastName}`,
       email: emailAddresses[0].emailAddress,
       avatar: imageUrl,
       color: getUserColor(id),
-    },
-  };
+    }
+  }
 
-  // Start an auth session inside your endpoint
-  const session = liveblocks.prepareSession(
-    user.id,
-    { userInfo: user.info } // Optional
+  // Identify the user and return the result
+  const { status, body } = await liveblocks.identifyUser(
+    {
+      userId: user.info.email,
+      groupIds: [],
+    },
+    { userInfo: user.info },
   );
 
-  // Use a naming pattern to allow access to rooms with wildcards
-  // Giving the user read access on their org, and write access on their group
-  session.allow(`${user.organization}:*`, session.READ_ACCESS);
-  session.allow(`${user.organization}:${user.group}:*`, session.FULL_ACCESS);
-
-  // Authorize the user and return the result
-  const { status, body } = await session.authorize();
   return new Response(body, { status });
 }
